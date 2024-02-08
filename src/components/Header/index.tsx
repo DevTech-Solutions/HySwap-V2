b6be099a2b5b6088260ee8fb7b1cb3cf0f2b7c2e
Igor Smolkov <smolkov.igor@gmail.com>
2024-02-08 17:32:10 +0300

    minimal changes to add network

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -131,7 +131,8 @@ const NETWORK_LABELS: { [chainId in ChainId]: string | null } = {
   [ChainId.RINKEBY]: 'Rinkeby',
   [ChainId.ROPSTEN]: 'Ropsten',
   [ChainId.GÖRLI]: 'Görli',
-  [ChainId.KOVAN]: 'Kovan'
+  [ChainId.KOVAN]: 'Kovan',
+  [ChainId.ZKEVMPOLYGON]: 'ZKEVMPOLYGON',
 }
 
 export default function Header() {
