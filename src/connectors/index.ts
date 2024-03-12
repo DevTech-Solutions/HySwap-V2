15c998153310a2a978e0ad135580c5a31994b774
Igor Smolkov <smolkov.igor@gmail.com>
2024-03-12 18:03:27 +0300

    remove unused

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,4 +1,3 @@
-import { Web3Provider } from '@ethersproject/providers'
 import { InjectedConnector } from '@web3-react/injected-connector'
 
 import { NetworkConnector } from './NetworkConnector'
@@ -14,11 +13,6 @@ if (typeof NETWORK_URL === 'undefined') {
 export const network = new NetworkConnector({
   urls: { [NETWORK_CHAIN_ID]: NETWORK_URL }
 })
-
-let networkLibrary: Web3Provider | undefined
-export function getNetworkLibrary(): Web3Provider {
-  return (networkLibrary = networkLibrary ?? new Web3Provider(network.provider as any))
-}
 
 export const injected = new InjectedConnector({
   supportedChainIds: [1442]
